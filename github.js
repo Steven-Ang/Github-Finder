50ebe5f9eadf7bb7b8f4b130875155d0df01a745
Steven-Ang <spikegreen2@gmail.com>
2018-09-07 16:59:23 +0800

    Fetch and display repos

--- github.js
+++ github.js
@@ -2,15 +2,21 @@ class GitHub {
   constructor() {
     this.client_id = "bedeb1654e272cb27c79";
     this.client_secret = "0a1115957859bbced701e2f8301bb149b63f1f48";
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
-    const profileData = await profileResponse.json();
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
 
     return {
-      profile: profileData
+      profile,
+      repos
     }
   }
 }
